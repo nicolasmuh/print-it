5f68775a0ce58e5879b0f6a66b28234dd6422855
nicolasmuh <143105933+nicolasmuh@users.noreply.github.com>
2024-02-14 15:53:02 +0100

    livrable
    index fonctionnel
    script commenté

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,2 @@
 # Print-It WebSite v.1
+remplacement de l'image fixe par carrousel avec deplacement au click
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// tableau image et titre
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,8 +17,10 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
+// création  indeximage
 let indeximage = 0;
-// creation et rajout des points 
+
+// création et rajout des points 
 document.addEventListener("DOMContentLoaded", function () {
 	let points = document.querySelector('.dots');
 	slides.forEach(function (slide, index) {
@@ -29,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-// creration des fleches
+// création des fleches
 const flechedroite = document.querySelector(".arrow_right");
 const flechegauche = document.querySelector(".arrow_left");
 // test
@@ -41,7 +44,7 @@ flechedroite.addEventListener ("click", function() {
 });
 
 
-// deplacement
+// fonction fleche gauche = reculer d'une image et passer de la 1ère a la dernière
 flechegauche.addEventListener ("click", ()=> {
 	if (indeximage == 0){
 		indeximage = slides.length -1;
@@ -51,6 +54,7 @@ flechegauche.addEventListener ("click", ()=> {
 
 	afficherBanner(slides[indeximage].image, slides[indeximage].tagLine, indeximage);
 });
+// fonction fleche droite avancer d'une image et passer de la dernière a la première
 flechedroite.addEventListener ("click", function() {
 	if (indeximage == slides.length -1) {
 		indeximage = 0;
@@ -60,33 +64,24 @@ flechedroite.addEventListener ("click", function() {
 	
 	afficherBanner(slides[indeximage].image, slides[indeximage].tagLine, indeximage);
 });
-
+// lancement de la fonction apres que le document soit chargé
 document.addEventListener("DOMContentLoaded", function () {
 
 	afficherBanner(slides[indeximage].image, slides[indeximage].tagLine, indeximage);
 });
-function afficherBanner (image, titre, index) {
-// zone carrousel slide et titre
-	let carrousel = document.getElementById("banner");
-	let slidecarrousel = document.querySelector (".banner-img");
-	let titrecarrousel = document.querySelector ("#banner p");
-	let prefix ='./assets/images/slideshow/';
 
-	slidecarrousel.src = prefix + image;
-	titrecarrousel.innerHTML = titre;
+// fonction slider parametre image, titre et index
+function afficherBanner (image, titre, index) {
+	let carrousel = document.getElementById("banner"); // définition de la zone du slider
+	let slidecarrousel = document.querySelector (".banner-img");	// définition de l'image du slide
+	let titrecarrousel = document.querySelector ("#banner p");		// définition du titre
+	let prefix ='./assets/images/slideshow/';						// définition adresse image
+
+	slidecarrousel.src = prefix + image;				// image adressée
+	titrecarrousel.innerHTML = titre;					//insertion du titre
 	const points = document.querySelectorAll('.dot');
 	points.forEach(element => {
         element.classList.remove('dot_selected');
 	})
-	points[index].classList.add('dot_selected');
+	points[index].classList.add('dot_selected');		// ajout du point selectionné sur l'image en cours
 }
-/**Changement de point actif
-    const points = document.querySelectorAll('.dot');
-
-    // Suppression du point actif au clic
-    points.forEach(element => {
-        element.classList.remove('dot_selected');
-    })
-
-	// Ajout du point actif au clic
-    points[indexSlideActive].classList.add('dot_selected');**/
